936210d1ea29a876c8a257f7d455b8f10809124b
dvberkel <daan.v.berkel.1980+git@gmail.com>
2013-12-04 16:04:43 +0100

    add a canvas

--- js/wish.js
+++ js/wish.js
@@ -1,3 +1,11 @@
 (function(){
-    console.log('loaded');
+    var body = document.getElementsByTagName('body')[0];
+
+    var canvas = document.createElement('canvas');
+    canvas.width = 640;
+    canvas.height= 640;
+    body.appendChild(canvas);
+
+    context = canvas.getContext('2d');
+    context.fillRect(0, 0, canvas.width, canvas.height);
 })();
