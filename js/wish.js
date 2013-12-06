232dd336a85bb4036fca482bed4179a93ac44351
dvberkel <daan.v.berkel.1980+git@gmail.com>
2013-12-06 08:08:43 +0100

    compute the multipliers

--- js/wish.js
+++ js/wish.js
@@ -20,18 +20,67 @@
     context.save();
 
     context.strokeStyle = 'white';
-    context.lineWidth = 2;
+    context.lineWidth = 1;
 
-    var unit = canvas.width/3;
+    var unit = canvas.width/81;
 
     context.beginPath();
     context.moveTo(0, 0);
-    [0, 1, -2, 1].forEach(function(angle){
+    [
+	0, 1, -2, 1, 1, 1, -2, 1, -2, 1, -2, 1, 1, 1, -2, 1,
+	1, 1, -2, 1, 1, 1, -2, 1, -2, 1, -2, 1, 1, 1, -2, 1,
+	-2, 1, -2, 1, 1, 1, -2, 1, -2, 1, -2, 1, 1, 1, -2, 1,
+	1, 1, -2, 1, 1, 1, -2, 1, -2, 1, -2, 1, 1, 1, -2, 1,
+
+	1, 1, -2, 1, 1, 1, -2, 1, -2, 1, -2, 1, 1, 1, -2, 1,
+	1, 1, -2, 1, 1, 1, -2, 1, -2, 1, -2, 1, 1, 1, -2, 1,
+	-2, 1, -2, 1, 1, 1, -2, 1, -2, 1, -2, 1, 1, 1, -2, 1,
+	1, 1, -2, 1, 1, 1, -2, 1, -2, 1, -2, 1, 1, 1, -2, 1,
+
+	-2, 1, -2, 1, 1, 1, -2, 1, -2, 1, -2, 1, 1, 1, -2, 1,
+	1, 1, -2, 1, 1, 1, -2, 1, -2, 1, -2, 1, 1, 1, -2, 1,
+	-2, 1, -2, 1, 1, 1, -2, 1, -2, 1, -2, 1, 1, 1, -2, 1,
+	1, 1, -2, 1, 1, 1, -2, 1, -2, 1, -2, 1, 1, 1, -2, 1,
+
+	1, 1, -2, 1, 1, 1, -2, 1, -2, 1, -2, 1, 1, 1, -2, 1,
+	1, 1, -2, 1, 1, 1, -2, 1, -2, 1, -2, 1, 1, 1, -2, 1,
+	-2, 1, -2, 1, 1, 1, -2, 1, -2, 1, -2, 1, 1, 1, -2, 1,
+	1, 1, -2, 1, 1, 1, -2, 1, -2, 1, -2, 1, 1, 1, -2, 1,
+    ].forEach(function(angle){
 	angle *= Math.PI/3;
 	context.rotate(angle)
 	context.translate(unit, 0);
 	context.lineTo(0, 0);
     });
+
+    context.stroke();
+
+    context.restore();
+
+    context.translate(0, canvas.height/2);
+
+    context.save();
+
+    context.strokeStyle = 'white';
+    context.lineWidth = 1;
+
+    var n = 4;
+    var unit = canvas.width/ Math.pow(3, n);
+
+    context.beginPath();
+    context.moveTo(0, 0);
+    var proto = [0, 1, -2, 1];
+    for (index = 0; index < Math.pow(4, n); index++) {
+	var m = index;
+	while (m > 0 && m % 4 == 0) {
+	    m = m / 4;
+	}
+	var i = proto[m % 4];
+	var angle = i * Math.PI/3;
+	context.rotate(angle)
+	context.translate(unit, 0);
+	context.lineTo(0, 0);
+    }
 
     context.stroke();
 
